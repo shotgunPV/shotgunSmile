dcc9e626d083b14b72d9c314e852d4ae7e776935
pizzlevintizzle <paul_vinten@hotmail.com>
2024-11-27 06:56:25 +0000

    latest changes
    flaarp

---
+++ assets/js/headerFooterManager.js
@@ -0,0 +1,37 @@
+class SpecialHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+        <nav id="main-nav">
+            <a href="index.html">Home</a>
+            <a href="about.html">About Us</a>
+            <a href="songs.html">Songs</a>
+            <a href="gigHistory.html">Gig History</a>
+            <a href="comingUp.html">Coming Up</a>
+            <a href="contact.html">Contact Us</a>
+        </nav>
+        `;
+    }
+}
+
+class SpecialFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+        <style>
+            .fa-facebook, .fa-youtube, .fa-soundcloud {
+                font-size: 200%; /* Increase the size of the icons */
+            }
+        </style>
+        <p>Punkhub! Proudly serving the best punk in Portsmouth to the best pubs in Portsmouth!</p>
+        <nav id="social-nav">
+            <a href="https://www.facebook.com/people/Punkhub/100093279042809/?_rdr" target="_blank"><i class="fab fa-facebook"></i></a>
+            <a href="https://www.youtube.com/playlist?list=PL5YYo0Ot8TXMCwemoJ1bxqQpMlxPWKFyG" target="_blank"><i class="fab fa-youtube"></i></a>
+            <a href="https://soundcloud.com/punkhub/sets/punkhub-demo-recordings-2024" target="_blank"><i class="fab fa-soundcloud"></i></a>
+        </nav>
+        `;
+    }
+}
+
+customElements.define('special-header', SpecialHeader);
+customElements.define('special-footer', SpecialFooter);
